76cf643a68b8dc61ccfeae55e1ee0726d63fc5a8
Kritika Mehta <kritika.mehta042@gmailcom>
2022-11-17 22:10:55 -0500

    working API

---
+++ js/main.js
@@ -0,0 +1,1 @@
+//need main.js file as this is client side code for event listener for the button
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+//created an obeject
+const rappers = {
+  "21 savage": {
+    age: 29,
+    birthDate: "April 21",
+    birthName: "Seyaa Bin",
+    birthLocation: "London, England",
+  },
+
+  "Chance the rapper": {
+    age: 34,
+    birthDate: "April 21",
+    birthName: "caaaca Bin",
+    birthLocation: "London, England",
+  },
+
+  "Chance the fob": {
+    age: 34,
+    birthDate: "April 21",
+    birthName: "aaaaSeyaa Bin",
+    birthLocation: "London, England",
+  },
+
+  "other rap city the rapper": {
+    age: 34,
+    birthDate: "April 21",
+    birthName: "Saaaaaeyaa Bin",
+    birthLocation: "London, England",
+  },
+};
+//inside express server do a get request
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//listen to a diff path
+// have a callback with request and response parameter
+//send JSON as response
+app.get("/api/:rapperName", (request, response) => {
+  // grab query parament from request
+  const rapperName = request.params.rapperName.toLowerCase();
+
+  if (rappers[rapperName]) {
+    response.json(rappers[rapperName.birthName]);
+  } else {
+    //send other if rapper doesn't exist
+    response.json(rappers["other rap city the rapper"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}! You better go!`);
+});

f58777386490851f31da4b91ebb83a0decd53878
Kritika Mehta <kritika.mehta042@gmailcom>
2022-11-17 22:23:24 -0500

    heroku changes

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get("/api/:rapperName", (request, response) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is running on ${PORT}! You better go!`);
 });

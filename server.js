33b0b0877ba464dcee42f923fffed6688ad1ca43
Kritika Mehta <kritika.mehta042@gmailcom>
2022-11-19 15:55:48 -0500

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const cors == require('cors')
+const cors = require("cors");
 const PORT = 8000;
 
+app.use(cors());
 //created an obeject
 const rappers = {
   "21 savage": {

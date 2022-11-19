5cb1c75f5e412ebd8015c4424dabe425c11ecec7
Kritika Mehta <kritika.mehta042@gmailcom>
2022-11-19 15:50:13 -0500

    client-side code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 //need main.js file as this is client side code for event listener for the button
+//client side code so API talks to the front end
+
+document.querySelector("button").addEventListener("click", apiRequest);
+
+async function apiRequest() {
+  const rapperName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://rap-names-api-testtt.herokuapp.com/api/${rapperName}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+    document.querySelector("h2").innerText = data.birthName;
+  } catch (error) {
+    console.log(error);
+  }
+}
